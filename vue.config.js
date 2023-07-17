332bf431ca114ed9d6a98ecbaa610823ea111221
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-07-17 20:59:37 +0300

    gh pages added

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/online_store_vue/'
+  publicPath: './online_store_vue'
 });
